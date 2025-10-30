df236ef896dcf6b98c137e65b01d297a2ba9b4e1
Shinjini Nag <125287325+shinjini1202@users.noreply.github.com>
2025-10-30 20:42:57 +0530

    Create Task2.py

---
+++ Assignment_5/Task2.py
@@ -0,0 +1,4 @@
+original_list=[1,2,3,4,5,6,7,8,9,10]
+print("Original list :",original_list)
+print("Extracted first five element:",original_list[0:5])
+print("Reversed extracted elements:",original_list[4::-1])
