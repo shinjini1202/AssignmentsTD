692a5cfdc65c0d182590ac134078941ff1b72a6b
Shinjini Nag <125287325+shinjini1202@users.noreply.github.com>
2025-10-30 19:02:01 +0530

    Update Task1.py

--- Assignment_3/Task1.py
+++ Assignment_3/Task1.py
@@ -1,1 +1,14 @@
-ababa
+def factorial(n):
+    if n<0:
+        print("Enter valid number")
+        return 0
+    if n==0:
+        return 1
+    result=1
+    for i in range(1,n+1):
+        result *=i
+    return result
+num=int(input("Enter a number: "))
+output=factorial(num)
+if (output>0):
+    print("Factorial of "+str(num)+" is "+str(output))
